7ce0ab22d94e592856b40149329c0c343ef742a4
Bastian Bräu <bastian@braeu.me>
2023-12-30 12:53:37 +0100

    Update worked today on tracker stop

--- src/js/src/lib/tracker.ts
+++ src/js/src/lib/tracker.ts
@@ -92,6 +92,7 @@ export class Tracker extends EventTarget {
     this.workedToday! += this.timeElapsed;
     this.render();
     this.dispatchEvent(new Event(TrackerEvent.stop));
+    this.dispatchEvent(new Event(TrackerEvent.requestWorkedToday)); // Prevent inconsistencies between local and persisted data
   }
 
   reset() {
